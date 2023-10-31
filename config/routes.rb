e587dd998f6008ae3d3521ce120c7ececc0682e3
ecurzola <146590441+ecurzola@users.noreply.github.com>
2023-10-31 05:56:19 +0000

    completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  
+  get("/rock", {:controller => "game", :action => "user_played_rock"})
+  get("/paper", {:controller => "game", :action => "user_played_paper"})
+  get("/scissors", {:controller => "game", :action => "user_played_scissors"})
+  get("/", {:controller => "game", :action => "homepage"})
 
 end
